65ce4428cb5348c81c54e5e6106328f274cc8f25
sacshop <sacshop@gmail.com>
2016-01-10 11:00:28 -0600

    morgana's choker trade fix

--- scripts/zones/Provenance/NPCs/reward.lua
+++ scripts/zones/Provenance/NPCs/reward.lua
@@ -975,7 +975,7 @@ if (trade:getItemCount() == 1) then
    player:addItem(16268,1);
    player:messageSpecial(ITEM_OBTAINED,16268,1);
 
-   		elseif (trade:hasItemQty(16269,1)) and (dominion_note >= Morganas_Choker)
+   		elseif (trade:hasItemQty(11603,1)) and (dominion_note >= Morganas_Choker)
 			 then    --This is a check to see if the player has enough notes
               player:delCurrency("dominion_note", 3000);
    player:tradeComplete();
