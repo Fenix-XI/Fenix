6e34532b6c6387feb15dcdd28103afe880c8b2ed
sacshop <sacshop@gmail.com>
2016-01-17 13:59:35 -0600

    2-3 workaround fix

--- scripts/zones/Balgas_Dais/npcs/Goblin_Footprint.lua
+++ scripts/zones/Balgas_Dais/npcs/Goblin_Footprint.lua
@@ -1,10 +1,8 @@
 function onTrigger(player,npc)
 
-		if (player:hasKeyItem(DARK_KEY)) then
-			player:addKeyItem(KINDRED_CREST);
-			player:messageSpecial(KEYITEM_OBTAINED,KINDRED_CREST);
-			player:setVar("MissionStatus",9);
-			player:delKeyItem(DARK_KEY);
-			player:PrintToPlayer("Please continue on with your next mission! Add KI Kindred Crest_ delKI Dark Key");
-		end
-		end;
+    if (player:hasKeyItem(DARK_KEY)) then
+            player:addKeyItem(KINDRED_CREST);
+            player:messageSpecial(KEYITEM_OBTAINED,KINDRED_CREST);
+            player:setVar("MissionStatus",9);
+            player:delKeyItem(DARK_KEY);
+    end
--- scripts/zones/Waughroon_Shrine/npcs/Goblin_Footprint.lua
+++ scripts/zones/Waughroon_Shrine/npcs/Goblin_Footprint.lua
@@ -1,10 +1,9 @@
 function onTrigger(player,npc)
 
-		if (player:hasKeyItem(DARK_KEY)) then
-			player:addKeyItem(KINDRED_CREST);
-			player:messageSpecial(KEYITEM_OBTAINED,KINDRED_CREST);
-			player:setVar("MissionStatus",9);
-			player:delKeyItem(DARK_KEY);
-			player:PrintToPlayer("Please continue on with your next mission! Add KI Kindred Crest_ delKI Dark Key");
-		end
-		end;
+    if ((player:getCurrentMission(SANDORIA) == JOURNEY_TO_BASTOK2 or 
+        player:getCurrentMission(WINDURST) == THE_THREE_KINGDOMS_BASTOK2) and 
+        player:getVar("MissionStatus") == 10) then
+            player:addKeyItem(KINDRED_CREST);
+            player:messageSpecial(KEYITEM_OBTAINED,KINDRED_CREST);
+            player:setVar("MissionStatus",11);
+    end
