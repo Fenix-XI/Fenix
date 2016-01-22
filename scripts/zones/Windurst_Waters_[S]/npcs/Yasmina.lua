387b927de30536a4fa7511ebc71fe8398baae68c
sacshop <sacshop@gmail.com>
2016-01-21 20:09:19 -0600

    chocobo CS correction

--- scripts/zones/Windurst_Waters_[S]/npcs/Yasmina.lua
+++ scripts/zones/Windurst_Waters_[S]/npcs/Yasmina.lua
@@ -22,7 +22,10 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
+local level = player:getMainLvl();
+    if (player:hasKeyItem(CHOCOBO_LICENSE) and level >= 20) then
     player:startEvent(0x0006, 10, 10);
+	end
 end;
 
 -----------------------------------
@@ -41,5 +44,16 @@ end;
 function onEventFinish(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
+	
+	if (csid == 0x0006 and option == 0) then
+
+
+            local duration = 1800 + (player:getMod(MOD_CHOCOBO_RIDING_TIME) * 60)
+
+            player:addStatusEffectEx(EFFECT_CHOCOBO,EFFECT_CHOCOBO,1,0,duration,true);
+
+            player:setPos(320,-4,-41,192,95);
+        end
+    
 end;
 
