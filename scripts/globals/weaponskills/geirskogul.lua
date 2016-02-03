d51088c43e55384a2c0209b295cd4d5346a2b441
sacshop <sacshop@gmail.com>
2016-02-02 22:31:13 -0600

    Custom relic WS restore.

--- scripts/globals/weaponskills/geirskogul.lua
+++ scripts/globals/weaponskills/geirskogul.lua
@@ -19,13 +19,13 @@ require("scripts/globals/weaponskills");
 function onUseWeaponSkill(player, target, wsID)
 
     local params = {};
-    params.numHits = 1;
-    params.ftp100 = 3; params.ftp200 = 3; params.ftp300 = 3;
-    params.str_wsc = 0.0; params.dex_wsc = 0.0; params.vit_wsc = 0.0; params.agi_wsc = 0.6; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
-    params.crit100 = 0.0; params.crit200 = 0.0; params.crit300 = 0.0;
-    params.canCrit = false;
+    params.numHits = 2;
+    params.ftp100 = 2.25; params.ftp200 = 2.25; params.ftp300 = 2.25;
+    params.str_wsc = 0.20; params.dex_wsc = 0.60; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
+    params.crit100 = 0.30; params.crit200 = 0.40; params.crit300 = 0.60;
+    params.canCrit = True;
     params.acc100 = 0.0; params.acc200= 0.0; params.acc300= 0.0;
-    params.atkmulti = 1;
+    params.atkmulti = 1.2;
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
         params.dex_wsc = 0.8; params.agi_wsc = 0.0;
--- scripts/globals/weaponskills/namas_arrow.lua
+++ scripts/globals/weaponskills/namas_arrow.lua
@@ -23,8 +23,8 @@ function onUseWeaponSkill(player, target, wsID)
     params.str_wsc = 0.4; params.dex_wsc = 0.0; params.vit_wsc = 0.0;
     params.agi_wsc = 0.4; params.int_wsc = 0.0; params.mnd_wsc = 0.0;
     params.chr_wsc = 0.0;
-    params.crit100 = 0.0; params.crit200 = 0.0; params.crit300 = 0.0;
-    params.canCrit = false;
+    params.crit100 = 0.20; params.crit200 = 0.40; params.crit300 = 0.60;
+    params.canCrit = True;
     params.acc100 = 0.0; params.acc200= 0.0; params.acc300= 0.0;
     params.atkmulti = 1;
 
