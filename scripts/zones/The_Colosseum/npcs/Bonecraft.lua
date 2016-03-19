6b7fcc9dae5bf70882e2faf91d84b58869dde992
sacshop <sacshop@gmail.com>
2016-03-19 11:14:33 -0500

    Re implimented craft NPC items that were missing

--- scripts/zones/The_Colosseum/npcs/Bonecraft.lua
+++ scripts/zones/The_Colosseum/npcs/Bonecraft.lua
@@ -52,6 +52,9 @@ stock = {0x0370,1,	--Bone Chip
 0x632,1,	--Titanictus Shell
 0x520,1,	--Angel Skin
 0x6b7,1,    --Harajanite Shell
+0xB60,1,    --Bugard Horn
+0x718,1,    --Wyrm Horn
+
 }	
  
 showShop(player, STATIC, stock);
