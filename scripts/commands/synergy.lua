3f9353e3f0f408f22bbea9d10eaa46e3e389dd58
sacshop <sacshop@gmail.com>
2016-04-06 10:51:01 -0500

    Jail fix

--- scripts/commands/synergy.lua
+++ scripts/commands/synergy.lua
@@ -10,5 +10,15 @@ cmdprops =
     parameters = "iiii"
 };
 function onTrigger(player, x, y, z, zone)
-            player:setPos('-65', '0', '4', '198', '246');
-end
+local valor_point = player:getCurrency("valor_point");
+local targ = GetPlayerByName( target );
+    if (targ ~= nil) then
+        if (targ:getZoneID() ~= 131) then
+             player:setPos('-65', '0', '4', '198', '246');
+        else
+            player:PrintToPlayer( "You been Jailed for a reason, good try! Now please enjoy your stay a while longer.");
+        end
+    else
+        player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
+    end  
+end	
