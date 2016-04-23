4feb873ff95c7c36728e4c4026c802c196664566
Alice5150 <fluxion5150@gmail.com>
2016-04-22 18:42:46 -0700

    Change @craft to @fenix, position change

--- scripts/commands/fenix.lua
+++ scripts/commands/fenix.lua
@@ -8,6 +8,6 @@ cmdprops =
     permission = 0,
     parameters = "iiii"
 };
-function onTrigger(player)
-            player:PrintToPlayer( "@craft has been changed to @fenix!");
+function onTrigger(player, x, y, z, zone)
+            player:setPos('-10.082', '-9.962', '-9.285', '0', '71');
 end
