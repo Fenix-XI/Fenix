ccf93b2626573084cbbd6b112ef09fe8b05cb57f
Alice5150 <fluxion5150@gmail.com>
2016-04-22 19:05:10 -0700

    Random locations

--- scripts/commands/fenix.lua
+++ scripts/commands/fenix.lua
@@ -1,13 +1,17 @@
----------------------------------------------------------------------------------------------------
--- func: crafting & hangout
--- desc: Moves self to crafting location The Colosseum
----------------------------------------------------------------------------------------------------
-
 cmdprops =
 {
     permission = 0,
-    parameters = "iiii"
+    parameters = ""
 };
-function onTrigger(player, x, y, z, zone)
-            player:setPos('-10.082', '-9.962', '-9.285', '0', '71');
-end
+function onTrigger(player)
+            spawn = math.random((1),(4))
+			if (spawn==1) then
+            player:setPos('-9', '-9', '10', '0', '71'); --NW
+			elseif (spawn==2) then
+			player:setPos('-9.887', '-9.915', '-9.285', '0', '71'); --SW
+			elseif (spawn==3) then
+			player:setPos('10', '-9', '10', '0', '71'); --NE
+			else
+            player:setPos('10.088', '-10.000', '-9.285', '0', '71'); --SE
+			end
+end			
