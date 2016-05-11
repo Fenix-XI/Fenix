188fa1ece247265387f91b56b57929b0b086cb80
Fluxion <fluxion5150@gmail.com>
2016-05-10 18:30:39 -0700

    Update Gu_Dha_Effigy.lua
    Remove commented out line

--- scripts/zones/Dynamis-Bastok/mobs/Gu_Dha_Effigy.lua
+++ scripts/zones/Dynamis-Bastok/mobs/Gu_Dha_Effigy.lua
@@ -38,7 +38,7 @@ function onMobDeath(mob,killer,ally)
         npc:setPos(mob:getXPos(),mob:getYPos(),mob:getZPos());
         npc:setStatus(0);
 
-        --ally:launchDynamisSecondPart(); -- Spawn dynamis second part. Commented out, causing crash
+        ally:launchDynamisSecondPart();
     end
 
 end;
